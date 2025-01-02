b3beed0f4a545ecce141aab80f9dcf3d3f43a65c
Muhammad <bsmuhammad352@gmail.com>
2025-01-02 05:32:41 +0200

    6th commit--on my birthday

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,4 @@
+//Initiating the App
 const initApp = () =>{
     const mobileMenu = document.getElementById('mob-menu');
     const cancelMoblieMenu = document.getElementById('mob-menu-cancel');
@@ -5,12 +6,14 @@ const initApp = () =>{
     const startQuizz = document.getElementById('start-quizz');
     const start = document.getElementById('start')
 
+    //This function controls hamburger menu icon appearance and disappearce, it also render the nagivation options window and cancel menu icon.
     const toggleMenu = () =>{
         mobileMenu.style.display = (mobileMenu.style.display === 'flex') ? 'none' : 'none';
         mobileMenuWind.style.display = (mobileMenuWind.style.display === 'none') ? "flex": "flex";
         cancelMoblieMenu.style.display = (cancelMoblieMenu.style.display === 'none') ? 'flex': 'none';
     }
 
+    //This function controls cancel menu icon appearance and disappearce, it also unload the nagivation options window and render the hamburger menu icon.
     const cancelMenu = () =>{
         mobileMenu.style.display = (mobileMenu.style.display === 'none') ? 'flex' : 'none';
         mobileMenuWind.style.display = (mobileMenuWind.style.display === 'flex') ? "none": "flex";
